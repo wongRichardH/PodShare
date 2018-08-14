afbed31de682ef3152b832bd08986f9549a3c104
RWONG <merichardwong@gmail.com>
2018-08-13 17:08:52 -0700

    Add login screen with Firebase Auth

--- PodShare/AppDelegate.swift
+++ PodShare/AppDelegate.swift
@@ -7,27 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
         self.window = UIWindow()
 
-        let recordVC = RecordVC()
-        let friendsVC = FriendsFeedVC()
+//        let recordVC = RecordVC()
+//        let friendsVC = FriendsFeedVC()
+//
+//        let tabBar = UITabBarController()
+//        tabBar.addChildViewController(recordVC)
+//        tabBar.addChildViewController(friendsVC)
+//
+//        self.window?.rootViewController = tabBar
 
-        let tabBar = UITabBarController()
-        tabBar.addChildViewController(recordVC)
-        tabBar.addChildViewController(friendsVC)
-
-        self.window?.rootViewController = tabBar
+        let loginRootVC = LoginVC()
+        self.window?.rootViewController = loginRootVC
+        
         self.window?.makeKeyAndVisible()
+
+        FirebaseApp.configure()
+
         return true
     }
 
